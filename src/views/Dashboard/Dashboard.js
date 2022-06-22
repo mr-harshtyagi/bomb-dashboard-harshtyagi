c3057bb78fd70e6311c1e18c26db0229eef00d8c
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-06-22 19:16:14 +0530

    add boardroom card ui

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import Page from '../../components/Page';
+import { ArrowUpwardSharp,ArrowDownwardSharp } from '@material-ui/icons';
 import { createGlobalStyle } from 'styled-components';
 import CountUp from 'react-countup';
 import CardIcon from '../../components/CardIcon';
@@ -16,6 +17,8 @@ import useTotalValueLocked from '../../hooks/useTotalValueLocked';
 //import { Bomb as bombProd } from '../../bomb-finance/deployments/deployments.mainnet.json';
 import { roundAndFormatNumber } from '../../0x';
 import MetamaskFox from '../../assets/img/metamask-fox.svg';
+import DiscordIcon from '../../assets/img/discord.svg';
+import DocsIcon from '../../assets/img/github.svg';
 import { Box, Button, Card, CardContent, Grid, Paper } from '@material-ui/core';
 import ZapModal from '../Bank/components/ZapModal';
 import { Alert } from '@material-ui/lab';
@@ -23,11 +26,10 @@ import { IoCloseOutline } from 'react-icons/io5';
 import { BiLoaderAlt } from 'react-icons/bi';
 import { makeStyles } from '@material-ui/core/styles';
 import useBombFinance from '../../hooks/useBombFinance';
-//import { ReactComponent as IconTelegram } from '../../assets/img/telegram.svg';
 import { Helmet } from 'react-helmet';
 import BombImage from '../../assets/img/bomb.png';
+import BShareImage from '../../assets/img/bshare-512.png';
 
-//import useBombMaxiStats from '../../hooks/useBombMaxiStats';
 
 import HomeImage from '../../assets/img/background.jpg';
 const BackgroundImage = createGlobalStyle`
@@ -38,13 +40,6 @@ const BackgroundImage = createGlobalStyle`
   }
 `;
 const TITLE = 'Investment Dashboard';
-
-// const BackgroundImage = createGlobalStyle`
-//   body {
-//     background-color: grey;
-//     background-size: cover !important;
-//   }
-// `;
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -63,15 +58,6 @@ const Home = () => {
   const bShareStats = usebShareStats();
   const tBondStats = useBondStats();
   const bombFinance = useBombFinance();
-  // const bombmaxi = useBombMaxiStats('0xd6f52e8ab206e59a1e13b3d6c5b7f31e90ef46ef000200000000000000000028');
-
-  // console.log(bombmaxi);
-  // let bomb;
-  // if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  //   bomb = bombTesting;
-  // } else {
-  //   bomb = bombProd;
-  // }
 
   const buyBombAddress = //'https://app.1inch.io/#/56/swap/BTCB/BOMB';
     //  'https://pancakeswap.finance/swap?inputCurrency=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&outputCurrency=' +
@@ -154,23 +140,6 @@ const Home = () => {
     setVideoLoading(!videoLoading);
   };
 
-  // const [onPresentIntroVid] = useModal(
-  //   <grid>
-  //     <Paper>
-  //       <div>
-  //         <iframe
-  //           width="560"
-  //           height="315"
-  //           src="https://www.youtube.com/embed/nhCWmmRNNhc"
-  //           title="YouTube video player"
-  //           frameborder="0"
-  //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  //           allowfullscreen
-  //         ></iframe>
-  //       </div>
-  //     </Paper>
-  //   </grid>,
-  // );
 
   return (
     <>
@@ -178,13 +147,182 @@ const Home = () => {
         <title>{TITLE}</title>
       </Helmet>
       <BackgroundImage />
-      <Grid item xs={12} sm={4}>
-        <Card style={{ paddingTop: '10px' }}>
-          <CardContent align="center">
-            <h2>Total Value Locked</h2>
-            <CountUp style={{ fontSize: '25px' }} end={TVL} separator="," prefix="$" />
-          </CardContent>
-        </Card>
+
+      <Grid container spacing={3} style={{ padding: '20px 10% 20px 10%' }}>
+        {/* Bomb Finance Summary */}
+        <Grid xs={12} style={{ marginBottom: '12px' }}>
+          <Paper style={{ background: 'rgba(32, 37, 67, 0.5)', height: '289px', borderRadius: '5px' }}>
+            <Box p={4} style={{ textAlign: 'center' }}>
+              <h1>Bomb Finance Summary</h1>
+              <p>
+                <strong>BOMB is pegged via algorithm to a 10,000:1 ratio to BTC. $100k BTC = $10 BOMB PEG</strong>
+              </p>
+              <p>
+                <h2>Best Algocoin | 0.5%+ DAILY | Audited | Doxxed team</h2>
+              </p>
+            </Box>
+          </Paper>
+        </Grid>
+
+        {/* BoardRoom */}
+        <Grid item xs={12} sm={8} style={{ marginBottom: '20px' }}>
+          <Box p={2} style={{ textAlign: 'center' }}>
+            <a
+              href="/strategy"
+              style={{ float: 'right', color: '#9EE6FF', textDecoration: 'underline', marginBottom: '10px' }}
+            >
+              Read Investement Strategy
+            </a>
+            <Button
+              style={{
+                color: 'white',
+                background:
+                  'radial-gradient(59345.13% 4094144349.28% at 39511.5% -2722397851.45%, rgba(0, 245, 171, 0.5) 0%, rgba(0, 173, 232, 0.5) 100%)',
+                width: '100%',
+                marginBottom: '15px',
+              }}
+            >
+              <strong>Invest Now</strong>
+            </Button>
+            <Button
+              style={{
+                color: 'black',
+                background: 'rgba(255, 255, 255, 0.5)',
+                width: '45%',
+                marginRight: '2%',
+              }}
+            >
+              <img alt="discord icon" style={{ width: '20px', marginRight: '5px' }} src={DiscordIcon} />
+              <strong>Chat on Discord</strong>
+            </Button>
+            <Button style={{ color: 'black', background: 'rgba(255, 255, 255, 0.5)', width: '45%' }}>
+              <img alt="docs icon" style={{ width: '20px', marginRight: '5px' }} src={DocsIcon} />
+              <strong>Read Docs</strong>
+            </Button>
+          </Box>
+          <Paper style={{ background: 'rgba(32, 37, 67, 0.5)', borderRadius: '5px' }}>
+            <Box p={4} style={{ textAlign: 'left' }}>
+              <img
+                alt="b share image"
+                style={{ width: '50px', float: 'left', marginRight: '10px' }}
+                src={BShareImage}
+              />
+
+              <h3 style={{ color: 'white' }}>
+                BoardRoom
+                <span
+                  style={{
+                    color: 'white',
+                    fontSize: '0.7rem',
+                    padding: '3px',
+                    borderRadius: '3px',
+                    marginLeft: '20px',
+                    verticalAlign: 'center',
+                    backgroundColor: 'rgba(0, 232, 162, 0.5)',
+                  }}
+                >
+                  Recommended
+                </span>
+              </h3>
+              <p>
+                Stake BSHARE and earn BOMB every epoch
+                <span style={{ float: 'right' }}>
+                  TVL: <strong>${'1,008,430'}</strong>
+                </span>
+              </p>
+              <hr style={{ border: '0.5px solid rgba(195, 197, 203, 0.75)' }} />
+              <p style={{ float: 'right' }}>
+                Total Staked:
+                <span>
+                  <img alt="b share image" style={{ width: '13px', margin: '0 5px' }} src={BShareImage} />
+                  <strong>{'7232'}</strong>
+                </span>
+              </p>
+            </Box>
+            <Grid container spacing={4} style={{ textAlign: 'center' }}>
+              <Grid item xs={3} style={{ padding: '0' }}>
+                Daily returns:
+                <h2 style={{ color: 'white', marginTop: '10px' }}>2%</h2>
+              </Grid>
+              <Grid item xs={2} style={{ padding: '0', textAlign: 'left' }}>
+                Your Stake:
+                <p>
+                  <img alt="b share image" style={{ width: '20px' }} src={BShareImage} />
+                  6.0000
+                </p>
+                <p>≈ $1171.62</p>
+              </Grid>
+              <Grid item xs={2} style={{ padding: '0', textAlign: 'left' }}>
+                Earned:
+                <p>
+                  <img alt="b share image" style={{ width: '20px' }} src={BombImage} />
+                  1660.4413
+                </p>
+                <p>≈ $1171.62</p>
+              </Grid>
+              <Grid item xs={5} style={{ padding: '0' }}>
+                <Box style={{ textAlign: 'center', padding: '10px' }}>
+                  <Button style={{ width: '45%', border: 'solid 2px', borderRadius: '20px', marginRight: '10px' }}>
+                    Deposit <ArrowUpwardSharp />
+                  </Button>
+                  <Button style={{ width: '45%', border: 'solid 2px', borderRadius: '20px', marginRight: '10px' }}>
+                    Withdraw <ArrowDownwardSharp />
+                  </Button>
+                  <Button
+                    style={{
+                      width: '90%',
+                      border: 'solid 2px',
+                      borderRadius: '20px',
+                      marginTop: '10px',
+                      marginRight: '10px',
+                    }}
+                  >
+                    Claim Rewards
+                    <img alt="b share image" style={{ width: '15px', marginLeft: '5px' }} src={BShareImage} />
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+
+        {/* Latest News */}
+        <Grid item xs={12} sm={4} style={{ marginBottom: '20px' }}>
+          <Paper style={{ background: 'rgba(32, 37, 67, 0.5)', height: '420px', borderRadius: '5px' }}>
+            <Box p={4} style={{ textAlign: 'left' }}>
+              <h3 style={{ color: 'white' }}>Latest News</h3>
+            </Box>
+          </Paper>
+        </Grid>
+
+        {/* Bomb Farms */}
+        <Grid xs={12} style={{ marginBottom: '20px' }}>
+          <Paper style={{ background: 'rgba(32, 37, 67, 0.5)', height: '350px', borderRadius: '5px' }}>
+            <Box p={4} style={{ textAlign: 'center' }}>
+              <h1>Bomb Finance Summary</h1>
+              <p>
+                <strong>BOMB is pegged via algorithm to a 10,000:1 ratio to BTC. $100k BTC = $10 BOMB PEG</strong>
+              </p>
+              <p>
+                <h2>Best Algocoin | 0.5%+ DAILY | Audited | Doxxed team</h2>
+              </p>
+            </Box>
+          </Paper>
+        </Grid>
+        {/* Bonds */}
+        <Grid xs={12}>
+          <Paper style={{ background: 'rgba(32, 37, 67, 0.5)', height: '150px', borderRadius: '5px' }}>
+            <Box p={4} style={{ textAlign: 'center' }}>
+              <h1>Bomb Finance Summary</h1>
+              <p>
+                <strong>BOMB is pegged via algorithm to a 10,000:1 ratio to BTC. $100k BTC = $10 BOMB PEG</strong>
+              </p>
+              <p>
+                <h2>Best Algocoin | 0.5%+ DAILY | Audited | Doxxed team</h2>
+              </p>
+            </Box>
+          </Paper>
+        </Grid>
       </Grid>
     </>
   );
